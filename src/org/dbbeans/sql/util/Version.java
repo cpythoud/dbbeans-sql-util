cd671439335e55a3721ce35fb4399c18b1069c3e
cpythoud <git@pythoud.net>
2017-01-01 10:55:29 -0700

    Added files to build project with ant.

---
+++ src/org/dbbeans/sql/util/Version.java
@@ -0,0 +1,26 @@
+package org.dbbeans.sql.util;
+
+public class Version {
+
+    private static final String  PREFIX  = "";
+    private static final int     MAJOR   = 0;
+    private static final int     MINOR   = 9;
+    private static final int     PATCH   = 0;
+    private static final String  POSTFIX = "-beta";
+
+    private static final String VERSION = PREFIX + MAJOR + "." + MINOR + "." + PATCH + POSTFIX;
+
+    /**
+     * @return the version number of this library
+     */
+    public static String get() {
+        return VERSION;
+    }
+
+    /**
+     * To be used in build scripts.
+     */
+    public static void main (String[] args) {
+        System.out.println(get());
+    }
+}
